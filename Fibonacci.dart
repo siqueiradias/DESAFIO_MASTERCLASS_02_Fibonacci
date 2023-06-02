04b2f2a34c018b91fcedce132d50f3b5866526ba
siqueiradias <diogosdias@outlook.com>
2023-06-02 12:05:33 +0100

    Finished

--- Fibonacci.dart
+++ Fibonacci.dart
@@ -1,31 +1,60 @@
+import 'dart:io';
+
 void main() {
-  double valor = 99;
-  List serieFibonacci = fibonacci(valor: valor);
-  double totalFibonacci = somar_lista(serieFibonacci);
+  print("\x1B[2J\x1B[0;0H"); //Limpa a tela
+  print('Informe um valor: ');
+  int valor = receberValor();
 
-  print('Serie de Fibonacci: $serieFibonacci');
+  var serieFib = serieFibonacci(valores: [], n: valor);
+  double somaFibonacci =
+      somarLista(lista: serieFib, posicao: 0, somatorio: 0.0);
+
+  print('=' * 100);
+  print('Serie de Fibonacci: $serieFib');
   print('');
-  print('Somatorio da Serie de Fibonacci: $totalFibonacci');
+  print('Somatorio da Serie de Fibonacci: $somaFibonacci');
+  print('-' * 100);
 }
 
-//F(0)=0; F(1)=1; F(n) = F(n-1) + F(n-2), n>1
-List fibonacci({required double valor}) {
-  List fib = [];
-    for (var i = 0; i < valor-1; i++) {
-    if (i == 0){
-      fib.add(0.0);
-      fib.add(1.0);
+///Crie a Série de Fibonacci:
+///F(0)=0; F(1)=1; F(n) = F(n-1) + F(n-2), n>1
+dynamic serieFibonacci({required List valores, required int n}) {
+  if (n == 1) {
+    return valores;
+  } else {
+    if (valores.length < 2) {
+      valores.add(0.0);
+      valores.add(1.0);
     }
-    double x = fib[fib.length - 1] + fib[fib.length - 2];
-    fib.add(x);
+    double x = valores[valores.length - 1] + valores[valores.length - 2];
+    valores.add(x);
+    return serieFibonacci(valores: valores, n: n - 1);
   }
-  return fib;
 }
 
-double somar_lista (List lista){
-  double soma = 0;
-  for (var i = 0; i < lista.length; i++){
-    soma += lista[i];
+///Faz o somatório do conteúdo da lista  
+double somarLista(
+    {required List lista, required int posicao, required double somatorio}) {
+  if (posicao >= lista.length - 1) {
+    return somatorio;
+  } else {
+    posicao++;
+    somatorio += lista[posicao];
+    return somarLista(lista: lista, posicao: posicao, somatorio: somatorio);
   }
-  return soma;
+}
+
+///Trata a entrada de dados do usuario
+int receberValor() {
+  String? textValor = stdin.readLineSync();
+  int? x = int.tryParse(textValor!);
+  if ((x is int) && (x > 1)) {
+    return x;
+  } else {
+    print("\x1B[2J\x1B[0;0H"); //Limpa a tela
+    print("Valor invalido!");
+    print("Valor deve ser um número maior que '1'.");
+    print('Informe um valor: ');
+    return receberValor();
+  }
 }
