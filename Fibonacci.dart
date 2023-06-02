c703e5a1f0df6671f2ab5d453ebd5016f2f83c54
siqueiradias <diogosdias@outlook.com>
2023-06-02 10:49:56 +0100

    1st try

---
+++ Fibonacci.dart
@@ -0,0 +1,31 @@
+void main() {
+  double valor = 99;
+  List serieFibonacci = fibonacci(valor: valor);
+  double totalFibonacci = somar_lista(serieFibonacci);
+
+  print('Serie de Fibonacci: $serieFibonacci');
+  print('');
+  print('Somatorio da Serie de Fibonacci: $totalFibonacci');
+}
+
+//F(0)=0; F(1)=1; F(n) = F(n-1) + F(n-2), n>1
+List fibonacci({required double valor}) {
+  List fib = [];
+    for (var i = 0; i < valor-1; i++) {
+    if (i == 0){
+      fib.add(0.0);
+      fib.add(1.0);
+    }
+    double x = fib[fib.length - 1] + fib[fib.length - 2];
+    fib.add(x);
+  }
+  return fib;
+}
+
+double somar_lista (List lista){
+  double soma = 0;
+  for (var i = 0; i < lista.length; i++){
+    soma += lista[i];
+  }
+  return soma;
+}
---
+++ Fibonacci_rec.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  
+}
+
+dynamic somar (List valores, int n){
+  if (n == 1){
+    return valores;
+  } else{
+    return n;
+  }
+}
